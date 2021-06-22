7500a5b63826cfe158401ad80aa726c02470fc43
Avivit Weissman <wavivit@gmail.com>
2021-06-21 22:55:47 -0700

    Exercise 1 - created 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,29 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+class Store < ActiveRecord::Base
+
+end
+
+store1 = Store.create do |s|
+  s.name = "Burnaby"
+  s.annual_revenue = 300000
+  s.mens_apparel = true
+  s.womens_apparel = true
+end
+
+store2 = Store.create do |s|
+  s.name = "Richmond"
+  s.annual_revenue = 1260000
+  s.mens_apparel = false
+  s.womens_apparel = true
+end
+
+store3 = Store.create do |s|
+  s.name = "Gastown"
+  s.annual_revenue = 190000
+  s.mens_apparel = true
+  s.womens_apparel = false
+end
+
+puts "#{Store.count} stores in the Db"
