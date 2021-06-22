c1eb08256b67465433e537db4b65ac2cdc3d532d
Avivit Weissman <wavivit@gmail.com>
2021-06-22 11:14:26 -0700

    Exercise 5 Calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,8 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "The annual revenue of all the stores is #{Store.sum("annual_revenue")} "
+puts "The Average annual revenue of all the stores is #{Store.average("annual_revenue")} "
+
+store_more_1m = Store.where("annual_revenue >= ?" , 1000000).count
+puts "There are #{store_more_1m} stores that are generating $1M or more in annual sales"
