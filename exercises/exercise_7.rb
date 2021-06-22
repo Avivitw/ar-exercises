54b5aa943bcd2fa1d3aa320ccd1f23a5e7b820fb
Avivit Weissman <wavivit@gmail.com>
2021-06-22 14:47:53 -0700

    Exercise 7 Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,12 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+  puts "Please type a store name"
+  store_name = gets.chomp
+
+storeX = Store.create do |s|
+  s.name = store_name
+end
+
+puts "Is it valid? #{storeX.valid?}"
+puts "The Error is: #{storeX.errors.full_messages}"
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,8 @@
 class Employee < ActiveRecord::Base
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, presence: true, :inclusion => 40..200
+  validates :store_id, presence: true
+  
 end
